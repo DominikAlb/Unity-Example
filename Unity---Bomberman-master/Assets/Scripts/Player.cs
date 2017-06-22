7de0cbe7bbfc7198c1c90bd5fd69b8efb881affa
piesFM <dominik.albiniak@o2.pl>
2017-06-22 21:41:20 +0200

    example

---
+++ Unity---Bomberman-master/Assets/Scripts/Player.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public int              playerNumber;
+    public float            playerSpeed;
+    public ColisionTrigger  colTriUp;
+    public ColisionTrigger  colTriDown;
+    public ColisionTrigger  colTriLeft;
+    public ColisionTrigger  colTriRight;
+
+	public GameObject 		bombermanModel;
+	public GameObject 		bombermanBoy;
+    public GameObject       bomb;
+
+    private MainController  mainController;
+    private HashIDs         hashIds;
+    private bool            moving;
+    private Vector3         target;
+    private int             bombsOnMap;
+    private int             bombsLimit;
+    private Animator        animator;
+
+    void Awake()
+    {
+        moving          = false;
+        mainController  = GameObject.FindGameObjectWithTag(Tags.mainController).GetComponent<MainController>();
+        bombsOnMap      = 0;
+        bombsLimit      = 1;
+        animator        = bombermanBoy.GetComponent<Animator>();
+        hashIds         = GameObject.FindGameObjectWithTag(Tags.mainController).GetComponent<HashIDs>();
+    }
+
+    void Update()
+    {
+        if (!moving)
+            WaitForMove();
+        else
+            Moving();
+
+        WaitForBomb();
+    }
+
+    void WaitForMove()
+    {
+        if (!colTriUp.bombTouch && !colTriUp.softWallTouch && !colTriUp.playerTouch && !colTriUp.hardWallTouch && Input.GetKey(mainController.playersKeys[playerNumber].up))
+        {
+            moving = true;
+            target = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1f);
+			bombermanBoy.transform.localRotation = Quaternion.Euler (0f, 0f, 0f);
+            animator.SetBool(hashIds.walkingBool, true);
+        }
+
+        if (!colTriDown.bombTouch && !colTriDown.softWallTouch && !colTriDown.playerTouch && !colTriDown.hardWallTouch && Input.GetKey(mainController.playersKeys[playerNumber].down))
+        {
+            moving = true;
+            target = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1f);
+			bombermanBoy.transform.rotation = Quaternion.Euler (0f, 180f, 0f);
+            animator.SetBool(hashIds.walkingBool, true);
+        }
+
+        if (!colTriLeft.bombTouch && !colTriLeft.softWallTouch && !colTriLeft.playerTouch && !colTriLeft.hardWallTouch && Input.GetKey(mainController.playersKeys[playerNumber].left))
+        {
+            moving = true;
+            target = new Vector3(transform.position.x - 1f, transform.position.y, transform.position.z);
+			bombermanBoy.transform.localRotation = Quaternion.Euler (0f, 270f, 0f);
+            animator.SetBool(hashIds.walkingBool, true);
+        }
+
+        if (!colTriRight.bombTouch && !colTriRight.softWallTouch && !colTriRight.playerTouch && !colTriRight.hardWallTouch && Input.GetKey(mainController.playersKeys[playerNumber].right))
+        {
+            moving = true;
+            target = new Vector3(transform.position.x + 1f, transform.position.y, transform.position.z);
+			bombermanBoy.transform.localRotation = Quaternion.Euler (0f, 90f, 0f);
+            animator.SetBool(hashIds.walkingBool, true);
+        }
+    }
+
+    void Moving()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, playerSpeed * Time.deltaTime);
+
+        if (transform.position == target)
+        {
+            moving = false;
+            StartCoroutine(StopMovingAnimation());
+        }
+
+    }
+
+    IEnumerator StopMovingAnimation()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        if(!moving)
+            animator.SetBool(hashIds.walkingBool, false);
+    }
+
+    void WaitForBomb()
+    {
+        if(!moving && Input.GetKeyDown(mainController.playersKeys[playerNumber].putBomb) && bombsOnMap < bombsLimit)
+        {
+            GameObject bombObj = Instantiate(bomb, transform.position, Quaternion.identity) as GameObject;
+            bombObj.GetComponent<Bomb>().SetUpOwner(gameObject, 1);
+            ++bombsOnMap;
+        }
+    }
+
+    public void ReturnOneBomb()
+    {
+        --bombsOnMap;
+    }
+}
